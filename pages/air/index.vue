5943de92feebf3c1265090d4a5901d7362586341
黄辉 <53460222@qq.com>
2019-09-06 17:42:38 +0800

    特价机票列表显示完成

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -36,7 +36,17 @@
     </h2>
     <!-- 特价机票 -->
     <div class="air-sale">
-        
+        <el-row type="flex" class="air-sale-pic" justify="space-between">
+            <el-col :span="6" v-for="(item, index) in sales" :key="index">
+                <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`">
+                    <img :src="item.cover"/>
+                    <el-row class="layer-bar" type="flex" justify="space-between">
+                        <span>{{item.departCity}}-{{item.destCity}}</span>
+                        <span>￥699</span>
+                    </el-row>
+                </nuxt-link>
+            </el-col>
+        </el-row>
     </div>
   </section>
 </template>
@@ -44,7 +54,20 @@
 <script>
 import searchtab from '../../components/air/search'
 export default {
-  
+  data(){
+    return{
+      sales: []
+    }
+  },
+ async mounted(){
+    console.log(1111);
+     let specialdata=await this.$axios({
+        url:'/airs/sale'
+      })
+      this.sales=specialdata.data.data
+      console.log(this.sales)
+
+  },
   components:{
   searchtab
   }
